1aa3c724cf34d5f8f0b0e52c83d43eb7bcda8521
igic01 <igic.nikola01@gmail.com>
2024-01-26 17:01:16 +0100

    first commit for this rep

---
+++ js/script_pagi.js
@@ -0,0 +1,28 @@
+// const all_pagis = Array.from(document.querySelectorAll(".pagi"));
+// const pagi = all_pagis.filter(item => item.querySelector(".dots"));
+
+// function make_dots() {
+//     pagi.forEach(item => {
+//         const blocks_count = item.querySelectorAll(".block").length;
+
+//         if (blocks_count === 1) {
+//             return;
+//         }
+
+//         const dots = item.querySelector(".dots");
+
+//         const first_dot = document.createElement("div");
+//         first_dot.classList.add("dot");
+//         first_dot.setAttribute('id', 'selected');
+//         dots.appendChild(first_dot);
+//         for (let i = 1; i < blocks_count; i++) {
+//             const dot = document.createElement("div");
+//             dot.classList.add("dot");
+//             dots.appendChild(dot);
+//         }
+//     });
+// }
+
+// make_dots(pagi);
+
+
