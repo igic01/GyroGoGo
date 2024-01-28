133f160c2db4dca2dbc97a4f895319b94f1d2512
igic01 <igic.nikola01@gmail.com>
2024-01-28 17:47:32 +0100

    last version with dots

--- js/script_pagi.js
+++ js/script_pagi.js
@@ -1,28 +1,48 @@
-// const all_pagis = Array.from(document.querySelectorAll(".pagi"));
-// const pagi = all_pagis.filter(item => item.querySelector(".dots"));
+const pagi = document.querySelectorAll(".pagi");
 
-// function make_dots() {
-//     pagi.forEach(item => {
-//         const blocks_count = item.querySelectorAll(".block").length;
+function make_dots() {
+    pagi.forEach(item => {
+        const cards_count = item.querySelectorAll(".card").length;
 
-//         if (blocks_count === 1) {
-//             return;
-//         }
+        if (cards_count <= 3) {
+            return;
+        }
 
-//         const dots = item.querySelector(".dots");
+        const dots_div = item.querySelector(".dots");
+        const dots = [];
 
-//         const first_dot = document.createElement("div");
-//         first_dot.classList.add("dot");
-//         first_dot.setAttribute('id', 'selected');
-//         dots.appendChild(first_dot);
-//         for (let i = 1; i < blocks_count; i++) {
-//             const dot = document.createElement("div");
-//             dot.classList.add("dot");
-//             dots.appendChild(dot);
-//         }
-//     });
-// }
+        const first_dot = document.createElement("div");
+        first_dot.classList.add("dot");
+        dots.push(first_dot);
+        first_dot.setAttribute('id', 'selected');
+        first_dot.setAttribute('order', '0');
+        first_dot.addEventListener("click", (e) => {
+            console.log(first_dot.getAttribute("order"));
+            selection(dots, first_dot);
+        });
+        dots_div.appendChild(first_dot);
+        for (let i = 1; i < cards_count; i++) {
+            const dot = document.createElement("div");
+            dots.push(dot)
+            dot.classList.add("dot");
+            dot.setAttribute('order', i);
+            dots_div.appendChild(dot);
+            dot.addEventListener("click", (e) => {
+                selection(dots, dot);
+            });
+        }
+    });
+}
 
-// make_dots(pagi);
+function selection(dots, selected){
+    dots.forEach(dot => {
+        dot.removeAttribute("id");
+    });
+    selected.setAttribute('id', 'selected');
+}
+
+make_dots(pagi);
+
+
 
 
