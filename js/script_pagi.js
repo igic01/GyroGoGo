4b29833ee9cef010b2b4b4a5f0dfd601d9eb5a9c
igic01 <igic.nikola01@gmail.com>
2024-01-31 19:15:20 +0100

    new idea for pagi and it solved next step footer

--- js/script_pagi.js
+++ js/script_pagi.js
@@ -1,45 +1,19 @@
 const pagis = document.querySelectorAll(".pagi");
 
-pagis.forEach( pagi =>{
-    const block = pagi.querySelector(".block");
-    const cards = block.querySelectorAll(".card");
-    const dash = block.clientWidth / 2.85;
-    const arrow = pagi.querySelectorAll(".arrow");
-    const arrow_left = arrow[0];
-    const arrow_right = arrow[1];
+pagis.forEach(pagi => {
+    const interface = pagi.querySelector(".interface");
+    const block = interface.querySelector(".block");
+    const card_width = block.querySelector(".card").offsetWidth;
+    const arrows = pagi.querySelectorAll("i");
+    const left_arrow = arrows[0];
+    const right_arrow = arrows[1];
 
-
-    arrow_left.addEventListener("click", ()=>{
-        console.log("left");
-        cards.forEach( card => {
-            const current_position = parseFloat(window.getComputedStyle(card).getPropertyValue('transform').split(',')[4]) || 0;
-            const new_position = current_position + dash;
-
-            card.style.transform = "translateX(" + new_position + "px)";
-        });
-
+    left_arrow.addEventListener("click", () => {
+        block.scrollLeft += -card_width;
     });
 
-    arrow_right.addEventListener("click", ()=>{
-        console.log("right");
-        cards.forEach( card => {
-            const current_position = parseFloat(window.getComputedStyle(card).getPropertyValue('transform').split(',')[4]) || 0;
-            const new_position = current_position - dash;
-
-            card.style.transform = "translateX(" + new_position + "px)";
-        });
-
-        setTimeout(() => {
-            console.log("test");
-            
-            cards.forEach( card => {
-                card.style.transition = 'none';
-                card.style.transform = "translateX(0px)";
-            });
-            cards[0].parentNode.removeChild(cards[0]);
-        }, 500) 
+    right_arrow.addEventListener("click", () => {
+        block.scrollLeft += card_width;
     });
+
 });
-
-
-
